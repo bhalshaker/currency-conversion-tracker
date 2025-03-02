00ebf4fa0d51edc5f12900c530bcea5be69ccd9c
Ebrahim Alshaker <alshaker@proton.me>
2025-03-02 06:43:32 +0300

    paths structure

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/transactions')
+def all_trancations():
     return 'Hello World'
+
+@app.route('transactions/<id>')
+def transactions_by_id():
+    return None
+
+@app.route('transactions/search')
+def transactions_search():
+    return None
 
 if __name__ == '__main__':
     app.run()
