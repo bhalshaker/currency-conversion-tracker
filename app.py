d1cfa86be3ea34ec2dd44d53fdfbe7246f67cdb0
Ebrahim Alshaker <alshaker@proton.me>
2025-03-07 19:08:13 +0300

    Catch 404 and 500 errors of the application

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def all_trancations():
     return 'Hello World'
 
+@app.route('/transactions/upload',methods=['POST'])
+def upload_transactions():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return jsonify(sr.response_status(200,'SUCCESSFUL','File was upload successfuly'))
+
+
 @app.route('/transactions/<id>')
 def transactions_by_id(id):
     try:
@@ -20,6 +28,16 @@ def transactions_search():
     id=request.args.get('id')
     print(id)
     return jsonify(sr.response_status())
+
+@app.errorhandler(404) 
+# inbuilt function which takes error as parameter 
+def not_found(e):
+    return jsonify(sr.response_status(404,'NOT FOUND','Sorry, requested URL/Resource was not found!'))
+
+@app.errorhandler(500) 
+# inbuilt function which takes error as parameter 
+def not_found(e):
+    return jsonify(sr.response_status(500,'ERROR','Sorry, an expected technical error occured!'))
 
 if __name__ == '__main__':
     app.run(debug=True)
