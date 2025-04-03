beb6112d4fe25baf30b0830529beba843399ccb2
Ebrahim Alshaker <alshaker@proton.me>
2025-04-03 23:13:39 +0300

    Create currency conversion request to the API

--- controller/convert_currency_controller.py
+++ controller/convert_currency_controller.py
@@ -9,3 +9,10 @@ def get_api_currencies():
 def does_currency_exists(currency,currency_list):
     """Check if currency in currency_list"""
     return currency in currency_list
+
+def convert_currency(to_currency,from_currency,transaction_date,amount):
+    """Convert from a currency to another based on date"""
+    request_url=f"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{transaction_date.strftime('%Y-%m-%d')}/v1/currencies/{to_currency.lower()}.json"
+    convert_request=requests.get(request_url)
+    convert_request.raise_for_status()
+    return convert_request.json()[from_currency.lower()]*amount
